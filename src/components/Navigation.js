967e4ffd6f59cf84e91868d64db279ef98f26fd9
mamadou sylla <m27sylla@gmail.com>
2020-06-12 20:10:28 +0200

    components knowledges done

---
+++ src/components/Navigation.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navigation = () =>
+{
+
+	return (
+		<div className="sidebar">
+
+			<div className="id">
+				<div className="idContent">
+					<img src="./media/billGates.jpg" alt="profil-pic" />
+					<h3>Jean Scratch</h3>
+				</div>
+			</div>
+
+			<div className="navigation">
+				<ul>
+					<li>
+						<NavLink exact to="/" activeClassName="navActive">
+							<i className="fas fa-home"></i>
+							<span>Accueil</span>
+						</NavLink>
+					</li>
+					<li>
+						<NavLink exact to="/knowledges" activeClassName="navActive">
+							<i className="fas fa-mountain"></i>
+							<span>Compétences</span>
+						</NavLink>
+					</li>
+					<li>
+						<NavLink exact to="/portfolio" activeClassName="navActive">
+							<i className="fas fa-images"></i>
+							<span>Portfolio</span>
+						</NavLink>
+					</li>
+					<li>
+						<NavLink exact to="/contact" activeClassName="navActive">
+							<i className="fas fa-address-book"></i>
+							<span>Contact</span>
+						</NavLink>
+					</li>
+				</ul>
+			</div>
+
+			<div className="socialNetwork">
+				<ul>
+					<li>
+						<a href="http://www.google.com" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i></a>
+					</li>
+					<li>
+						<a href="http://www.google.com" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a>
+					</li>
+					<li>
+						<a href="http://www.google.com" target="_blank" rel="noopener noreferrer"><i className="fab fa-twitter"></i></a>
+					</li>
+					<li>
+						<a href="http://www.google.com" target="_blank" rel="noopener noreferrer"><i className="fab fa-codepen"></i></a>
+					</li>
+				</ul>
+				<div className="signature">
+					<a href="https://www.youtube.com/channel/UCHGLV13U7YRbjrKpqfbtyYg/videos" target="_blank" rel="noopener noreferrer">
+						<p>fromScratch - 2020</p>
+					</a>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Navigation;
---
+++ src/components/knowledges/Experience.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Experience = () =>
+{
+	return (
+		<div className="experience">
+			<h3>Expérience</h3>
+			<div className="exp-1">
+				<h4>Directeur Technique - Apple</h4>
+				<h5>2018-2020</h5>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum cum eum, ipsum quae exercitationem explicabo temporibus deserunt officia amet esse perspiciatis commodi quidem autem sequi sunt atque? Facere, veritatis. Voluptatum ipsam quaerat explicabo, molestiae sequi labore ipsum eos id facilis?</p>
+			</div>
+			<div className="exp-2">
+				<h4>Conseiller Stratégique - Microsoft</h4>
+				<h5>2016-2018</h5>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel ad obcaecati quam enim aliquid optio nam alias, aspernatur, corporis blanditiis fugit dolorum nihil cupiditate facere commodi possimus voluptatum nostrum omnis voluptate vitae nobis. Maiores non similique ipsum laudantium voluptates tempora!</p>
+			</div>
+		</div>
+	);
+};
+
+export default Experience;
