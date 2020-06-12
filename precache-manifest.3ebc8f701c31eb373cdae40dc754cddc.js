5deecbf1e8e4609800218dce9bc294ee68c7bf61
mamadou sylla <m27sylla@gmail.com>
2020-06-12 18:57:49 +0200

    Updates

---
+++ precache-manifest.3ebc8f701c31eb373cdae40dc754cddc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d939345a10c01050909df5fdee001450",
+    "url": "/my-react-cv/index.html"
+  },
+  {
+    "revision": "b8c07733c2ff7164a78c",
+    "url": "/my-react-cv/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "235f7609b4a2f78d4a48",
+    "url": "/my-react-cv/static/js/2.62009761.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-react-cv/static/js/2.62009761.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8c07733c2ff7164a78c",
+    "url": "/my-react-cv/static/js/main.e8d85967.chunk.js"
+  },
+  {
+    "revision": "5c95a96403a64041ff29",
+    "url": "/my-react-cv/static/js/runtime-main.8555c64a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-react-cv/static/media/logo.5d5d9eef.svg"
+  }
+]);
