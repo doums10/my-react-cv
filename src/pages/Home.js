09bf7f07882b0b3f3ffa373f49e98352c0557d7d
mamadou sylla <m27sylla@gmail.com>
2020-09-11 22:51:57 +0200

    cv canadien changement

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,22 +4,26 @@ import Navigation from '../components/Navigation';
 const Home = () =>
 {
 	return (
-		<div className="home">
-			<Navigation />
-			<div className="homeContent">
-				<div className="content">
-					<h1>Sylla Mamadou</h1>
-					<h2>Développeur Front-end</h2>
-					<div className="pdf">
-						<a href="./media/CV-Mamadou-Sylla2019.pdf" target="_blank">Télécharger CV 🇫🇷</a>
-					</div>
-					<div className="pdf">
-						<a href="./media/Développeur front end.rtf" target="_blank">Télécharger CV 🇨🇦</a>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+    <div className="home">
+      <Navigation />
+      <div className="homeContent">
+        <div className="content">
+          <h1>Sylla Mamadou</h1>
+          <h2>Développeur Front-end</h2>
+          <div className="pdf">
+            <a href="./media/CV-Mamadou-Sylla2019.pdf" target="_blank">
+              Télécharger CV 🇫🇷
+            </a>
+          </div>
+          <div className="pdf">
+            <a href="./media/CV MrSylla.pdf" target="_blank">
+              Télécharger CV 🇨🇦
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
